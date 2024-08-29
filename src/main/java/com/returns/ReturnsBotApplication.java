b2d9a0f82f52bc253d75a648f133ad3ef8454f1c
Nikolay <Novizdarialokin@yandex.ru>
2024-08-29 22:33:12 +0600

    feat: Добавил ReturnsBotApplication

---
+++ src/main/java/com/returns/ReturnsBotApplication.java
@@ -0,0 +1,15 @@
+package com.returns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Nikolay Radzivon
+ * @Date 24.08.2024
+ */
+@SpringBootApplication
+public class ReturnsBotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReturnsBotApplication.class, args);
+    }
+}
